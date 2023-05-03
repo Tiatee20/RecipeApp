f9d9bace46593b03d34945a3186a63054e7f32b7
OzzyD919 <123012413+OzzyD919@users.noreply.github.com>
2023-05-02 21:12:17 -0700

    Update project2.py

--- project2.py
+++ project2.py
@@ -26,7 +26,13 @@ class Button:
 class RecipesWindow:
     def __init__(self, main_win):
         self.win = GraphWin("Recipes", 200, 600)
-        self.text = Text(Point(100, 50), "Recipes Window")
+
+    # Set the background image
+        background = Image(Point(100, 300), "recipes.gif")
+        background.draw(self.win)
+        self.text = Text(Point(100, 50), "Recipes")
+        self.text.setTextColor("white")
+        self.text.setStyle("bold")
         self.text.draw(self.win)
         self.breakfast_button = Button(self.win, Point(100, 150), 80, 20, "Breakfast")
         self.lunch_button = Button(self.win, Point(100, 250), 80, 20, "Lunch")
@@ -42,11 +48,11 @@ class RecipesWindow:
                 self.win.close()
                 return
             elif self.breakfast_button.clicked(p):
-                open_breakfast_window(self.win)
+                breakfastWindow(self.win)
             elif self.lunch_button.clicked(p):
-                open_lunch_window(self.win)
+                lunchWindow(self.win)
             elif self.dinner_button.clicked(p):
-                open_dinner_window(self.win)
+                dinnerWindow(self.win)
         self.win.close()
 
 def open_recipes_window(main_win):
@@ -54,22 +60,30 @@ def open_recipes_window(main_win):
     recipes_window = RecipesWindow(main_win)
     recipes_window.run()
 
-def open_breakfast_window(main_win):
+class OpenRecipes:
+    def __init__(self, main_win):
+        self.win = GraphWin("Breakfast Recipes", 200, 600)
+        background = Image(Point(100, 300), "breakfast.gif")
+        background.draw(self.win)
+        self.text = Text(Point(100, 50), "Breakfast Recipes")
+        self.text.setStyle("bold")
+        self.text.draw(self.win)
+        self.pancake_button = Button(self.win, Point(100, 150), 80, 20, "Pancakes")
+        self.burrito_button = Button(self.win, Point(100, 250), 130, 20, "Breakfast Burritos")
+        self.back_button = Button(self.win, Point(100, 500), 40, 20, "Back")
+
+
+def breakfastWindow(main_win):
     """Open a new window for breakfast themed recipes."""
-    win = GraphWin("Breakfast Recipes", 200, 600)
-    text = Text(Point(100, 50), "Breakfast Recipes")
-    text.draw(win)
-    pancake_button = Button(win, Point(100, 150), 80, 20, "Pancakes")
-    burrito_button = Button(win, Point(100, 250), 130, 20, "Breakfast Burritos")
-    back_button = Button(win, Point(100, 500), 40, 20, "Back")
+    open_recipes = OpenRecipes(main_win)
     while True:
-        p = win.getMouse()
+        p = open_recipes.win.getMouse()
         if p is None:
             break
-        elif back_button.clicked(p):
-            win.close()
+        elif open_recipes.back_button.clicked(p):
+            open_recipes.win.close()
             return
-        elif pancake_button.clicked(p):
+        elif open_recipes.pancake_button.clicked(p):
             with open('Pancakes.txt', 'r') as f:
                 recipe = f.read()
             recipe_window = GraphWin("Pancake Recipe", 850, 600)
@@ -86,7 +100,7 @@ def open_breakfast_window(main_win):
                     recipe_window.close()
                     break
             recipe_window.close()
-        elif burrito_button.clicked(p):
+        elif open_recipes.burrito_button.clicked(p):
             with open('BB.txt', 'r') as f:
                 recipe = f.read()
             recipe_window = GraphWin("Breakfast Burrito Recipe", 1100, 600)
@@ -103,16 +117,20 @@ def open_breakfast_window(main_win):
                     recipe_window.close()
                     break
             recipe_window.close()
-    win.close()
+    open_recipes.win.close()
 
-def open_lunch_window(main_win):
+def lunchWindow(main_win):
     """Open a new window for lunch themed recipes."""
-    win = GraphWin("Lunch Recipes", 200, 600)
+    win = GraphWin("Lunch Recipes", 200, 500)
+    background = Image(Point(100, 200), "lunch.gif")
+    background.draw(win)
     text = Text(Point(100, 50), "Lunch Recipes")
+    text.setTextColor("black")
+    text.setStyle("bold")
     text.draw(win)
     GCS_button = Button(win, Point(100, 150), 155, 20, "Grilled Chicken Salad")
     GSW_button = Button(win, Point(100, 250), 135, 20, "Greek Salad Wrap")
-    back_button = Button(win, Point(100, 500), 40, 20, "Back")
+    back_button = Button(win, Point(100, 450), 40, 20, "Back")
     while True:
         p = win.getMouse()
         if p is None:
@@ -156,14 +174,17 @@ def open_lunch_window(main_win):
             recipe_window.close()
     win.close()
 
-def open_dinner_window(main_win):
+def dinnerWindow(main_win):
     """Open a new window for dinner themed recipes."""
-    win = GraphWin("Dinner Recipes", 250, 600)
+    win = GraphWin("Dinner Recipes", 250, 500)
+    background = Image(Point(100, 200), "dinner.gif")
+    background.draw(win)
     text = Text(Point(125, 50), "Dinner Recipes")
+    text.setStyle("bold")
     text.draw(win)
     SMB_button = Button(win, Point(125, 150), 175, 20, "Spaghetti and Meatballs")
     BSWA_button = Button(win, Point(125, 250), 225, 20, "Baked Salmon with Asparagus")
-    back_button = Button(win, Point(125, 500), 40, 20, "Back")
+    back_button = Button(win, Point(125, 450), 40, 20, "Back")
     while True:
         p = win.getMouse()
         if p is None:
@@ -207,42 +228,24 @@ def open_dinner_window(main_win):
             recipe_window.close()
     win.close()
 
-def open_add_window(main_win):
-    """Open a new window for adding recipes."""
-    win = GraphWin("Recipes", 200, 600)
-    text = Text(Point(100, 50), "Recipes Window")
-    text.draw(win)
-    breakfast_button = Button(win, Point(100, 150), 80, 20, "Breakfast")
-    lunch_button = Button(win, Point(100, 250), 80, 20, "Lunch")
-    dinner_button = Button(win, Point(100, 350), 80, 20, "Dinner")
-    back_button = Button(win, Point(100, 500), 40, 20, "Back")
-    while True:
-        p = win.getMouse()
-        if p is None:
-            break
-        elif back_button.clicked(p):
-            win.close()
-            return
-        elif breakfast_button.clicked(p):
-            print("Breakfast recipes")
-        elif lunch_button.clicked(p):
-            print("Lunch recipes")
-        elif dinner_button.clicked(p):
-            print("Dinner recipes")
-    win.close()
-
 def main():
     # create the main window
     win = GraphWin("Main Window", 300, 300)
 
+    # set the background image
+    background = Image(Point(150, 150), "main.gif")
+    background.draw(win)
+
     # title
     title = Text(Point(150, 50), "Recipe Book")
+    title.setSize(30)
+    title.setStyle("bold")
+    title.setTextColor("white")
     title.draw(win)
 
     # create the buttons
-    quit_button = Button(win, Point(50, 150), 40, 20, "Quit")
-    recipes_button = Button(win, Point(150, 150), 60, 20, "Recipes")
-    add_button = Button(win, Point(250, 150), 40, 20, "Add")
+    quit_button = Button(win, Point(100, 150), 40, 20, "Quit")
+    recipes_button = Button(win, Point(200, 150), 60, 20, "Recipes")
 
     # wait for user to click a button or close the window
     while True:
@@ -252,9 +255,6 @@ def main():
             break
         elif recipes_button.clicked(p):
             open_recipes_window(win)
-        elif add_button.clicked(p):
-            open_add_window(win)
-
 
 if __name__ == '__main__':
     main()
